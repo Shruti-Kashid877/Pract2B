633868a841f9061af3e17c65050d083be59aea80
Shruti-Kashid877 <shrutikashid01@gmail.com>
2024-02-13 08:23:12 +0530

    2nd Commited in main branch

--- prac/Ty.java
+++ prac/Ty.java
@@ -2,17 +2,7 @@ Class Ty
 { 
   pubic static void main(String args[])
   {
-    int a = 10;
-    int b = 20;
-    if(a>b)
-    {
-      System.out.println("a is greater that b");
-    }
-    else
-    {
-      System.out.println("a is less that b");
-    }
-
+    System.out.println("Shruti Kashid");
   }
   
 }
