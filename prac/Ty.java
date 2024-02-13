0ae4133101267c055354a3dc41121f59f55c773a
Shruti-Kashid877 <shrutikashid01@gmail.com>
2024-02-13 08:19:18 +0530

    Commited in main branch

---
+++ prac/Ty.java
@@ -0,0 +1,18 @@
+Class Ty
+{ 
+  pubic static void main(String args[])
+  {
+    int a = 10;
+    int b = 20;
+    if(a>b)
+    {
+      System.out.println("a is greater that b");
+    }
+    else
+    {
+      System.out.println("a is less that b");
+    }
+
+  }
+  
+}
