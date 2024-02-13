effcafed1c478da2d228c0f4ecb93d3d82b8401a
Shruti-Kashid877 <shrutikashid01@gmail.com>
2024-02-13 07:51:50 +0530

    Initial commit

---
+++ file1.java
@@ -0,0 +1,8 @@
+Class file1
+{ 
+  pubic static void main(String args[])
+  {
+    System.out.println("Shruti Kashid 034");
+  }
+  
+}
---
+++ file3.java
@@ -0,0 +1,8 @@
+Class file3
+{ 
+  pubic static void main(String args[])
+  {
+    System.out.println("Shruti Kashid");
+  }
+  
+}
